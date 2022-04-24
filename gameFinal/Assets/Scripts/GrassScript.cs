b7e1cf35b5731f4836f315e8fef143d213726ba6
Keyvan Parsan <parsankeyvan@gmail.com>
2022-04-24 17:12:13 -0400

    added grass
    I wanted this to have a greater jumpforce but im not sure why it doesn't work

--- gameFinal/Assets/Scripts/GrassScript.cs
+++ gameFinal/Assets/Scripts/GrassScript.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Platform : MonoBehaviour
+public class GrassScript : MonoBehaviour
 {
-    public float jumpForce = 50f;
+    public float jumpForce = 40f;
 
     private void onCollisionEnter2D(Collision2D collision)
     {
-        if(collision.relativeVelocity.y <= 0f)
+        if (collision.relativeVelocity.y <= 0f)
         {
             Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
             if (rb != null)
