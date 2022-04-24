eecb0e00402d80e8b8d00fe18cafe0a3f70c9b1d
Keyvan Parsan <parsankeyvan@gmail.com>
2022-04-24 18:52:09 -0400

    platforms get destroyed

---
+++ gameFinal/Assets/Scripts/DestroyPlatform.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPlatform : MonoBehaviour
+{
+    public GameObject PlatformDestructionPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlatformDestructionPoint = GameObject.Find("PlatformDestructionPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < PlatformDestructionPoint.transform.position.y)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
