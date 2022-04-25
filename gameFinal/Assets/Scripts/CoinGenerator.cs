1e0de744d096803ecdcab48902d65e41dd3e7de3
Keyvan Parsan <parsankeyvan@gmail.com>
2022-04-24 20:46:24 -0400

    audio edits

--- gameFinal/Assets/Scripts/CoinGenerator.cs
+++ gameFinal/Assets/Scripts/CoinGenerator.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CoinGenerator : MonoBehaviour
 {
+    public GameObject coinPrefab;
+
+    public int coinCount = 350;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 spawnPosition = new Vector3();
+
+        for (int i = 0; i < coinCount; i++)
+        {
+            spawnPosition.y += Random.Range(1f, 3f);
+            spawnPosition.x = Random.Range(-8f, 8f);
+            Instantiate(coinPrefab, spawnPosition, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
