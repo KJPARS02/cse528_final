5cdfd44d47265fcfe0ac235f4ec3bb5933d0013d
Kisan Patel <thekisanpatel@gmail.com>
2022-04-25 21:27:34 -0400

    IYKYK
    -Fixed bouncing to be constant
    -Implemented new player movement
    -Added Death
    -Death brings back to main menu

--- gameFinal/Assets/Scripts/Bounce.cs
+++ gameFinal/Assets/Scripts/Bounce.cs
@@ -1,29 +1,25 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Platform : MonoBehaviour
+public class Bounce : MonoBehaviour
 {
-<<<<<<< Updated upstream
-    public float jumpForce = 50f;
-=======
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
->>>>>>> Stashed changes
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
-        if (collision.gameObject.GetComponent<Rigidbody2D>().velocity.y <= 0)
+        
+        if(collision.gameObject.GetComponent<Rigidbody2D>().velocity.y <= 0)
         {
 
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 600f);
