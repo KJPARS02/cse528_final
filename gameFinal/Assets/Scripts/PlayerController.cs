0a4d78937f67587b00fbd9f1e3844fe9973116f9
Keyvan Parsan <parsankeyvan@gmail.com>
2022-04-26 20:05:16 -0400

    final additions

--- gameFinal/Assets/Scripts/PlayerController.cs
+++ gameFinal/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 10f;
     public Rigidbody2D rb;
     private float moveX;
+    float inputHorizontal;
+    float inputVertical;
     // Start is called before the first frame update
     void Awake()
     {
@@ -21,6 +23,21 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
+        if(inputHorizontal != 0)
+        {
+            rb.AddForce(new Vector2(inputHorizontal * moveSpeed, 0f));
+        }
+        if(inputHorizontal > 0)
+        {
+            gameObject.transform.localScale = new Vector3(.5f, .5f, .5f);
+        }
+        if (inputHorizontal < 0)
+        {
+            gameObject.transform.localScale = new Vector3(-.5f, .5f, .5f);
+        }
+
         Vector2 velocity = rb.velocity;
         velocity.x = moveX;
         rb.velocity = velocity;
